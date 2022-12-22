5c44e6554a8f37439f39876a2670f6e13d4b0097
mhstom <ec2-user@ip-172-31-61-239.ec2.internal>
2022-12-22 21:18:06 +0000

    Trigger deployment again

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ func main() {
 	log.Println("Service is started")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!!")
+		fmt.Fprintf(w, "Hello!!!!!")
 	})
 	if err := http.ListenAndServe("0.0.0.0:5000", nil); err != nil {
 		log.Fatal(err)
